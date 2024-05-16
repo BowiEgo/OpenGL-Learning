1183df4e3d7abd81599ca2cfefb12c42e0f2456b
bowiego <295030485@qq.com>
2024-05-16 09:43:33 +0800

    feat: Add Texture class and render a PNG texture to rectangle.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "src/IndexBuffer.h"
 #include "src/VertexArray.h"
 #include "src/Shader.h"
+#include "src/Texture.h"
 
 int main(void)
 {
@@ -42,10 +43,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     float positions[] = {
-        -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.5f,  0.5f,
-        -0.5f,  0.5f,
+        -0.5f, -0.5f, 0.0f, 0.0f, // 0
+         0.5f, -0.5f, 1.0f, 0.0f, // 1
+         0.5f,  0.5f, 1.0f, 1.0f, // 2
+        -0.5f,  0.5f, 0.0f, 1.0f  // 3
     };
 
     unsigned int indices[] = {
@@ -53,10 +54,15 @@ int main(void)
         2, 3, 0
     };
 
+    // Blending
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
     // VertexBuffer with VAO
     VertexArray va;
-    VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
     VertexBufferLayout layout;
+    layout.Push<float>(2);
     layout.Push<float>(2);
     va.AddBuffer(vb, layout);
 
@@ -67,6 +73,11 @@ int main(void)
     Shader shader("res/shaders/Basic.shader");
     shader.Bind();
     shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+    // Texture
+    Texture texture("res/textures/Avatar.png");
+    texture.Bind();
+    shader.SetUniform1i("u_Texture", 0);
 
     // Test unbind VertexArray, VertexBuffer, IndexBuffer and Shader
     va.Unbind();
--- src/Shader.h
+++ src/Shader.h
@@ -16,7 +16,7 @@ private:
     std::string m_FilePath;
     unsigned int m_RendererID;
     // caching for uniforms
-    std::unordered_map<std::string, unsigned int> m_UniformLocationCache;
+    std::unordered_map<std::string, int> m_UniformLocationCache;
 public:
     Shader(const std::string& filepath);
     ~Shader();
@@ -25,10 +25,11 @@ public:
     void Unbind() const;
 
     // Set uniforms
+    void SetUniform1i(const std::string& name, int value);
     void SetUniform1f(const std::string& name, float value);
     void SetUniform4f(const std::string& name, float v0, float v1, float f2, float f3);
 private:
-    unsigned int GetUniformLoaction(const std::string& name);
+    int GetUniformLoaction(const std::string& name);
     ShaderProgramSource ParseShader(const std::string& filepath);
     unsigned int CompileShader(unsigned int type, const std::string& source);
     unsigned int CreateShader(const std::string& vertextShader, const std::string& fragmentShader);
