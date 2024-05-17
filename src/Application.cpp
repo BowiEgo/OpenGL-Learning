39c6dad3753d80cb5ba409b1e3884d4dfe8ae021
bowiego <295030485@qq.com>
2024-05-17 16:27:55 +0800

    feat: Add Test framework and add a TestClearColor to application.

--- src/Application.cpp
+++ src/Application.cpp
@@ -16,6 +16,8 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
+#include "tests/TestClearColor.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -65,38 +67,6 @@ int main(void)
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-    // VertexBuffer with VAO
-    VertexArray va;
-    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-    VertexBufferLayout layout;
-    layout.Push<float>(2);
-    layout.Push<float>(2);
-    va.AddBuffer(vb, layout);
-
-    // IndexBuffer
-    IndexBuffer ib(indices, 6);
-
-    // Projection Matrix
-    glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-    // Shader
-    Shader shader("res/shaders/Basic.shader");
-    shader.Bind();
-    shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-    // shader.SetUniformMat4f("u_MVP", mvp);
-
-    // Texture
-    Texture texture("res/textures/Avatar.png");
-    texture.Bind();
-    shader.SetUniform1i("u_Texture", 0);
-
-    // Test unbind VertexArray, VertexBuffer, IndexBuffer and Shader
-    va.Unbind();
-    vb.Unbind();
-    ib.Unbind();
-    shader.Unbind();
-
     // Renderer
     Renderer renderer;
 
@@ -109,73 +79,24 @@ int main(void)
     ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
     ImGui_ImplOpenGL3_Init();
 
-    // ImGui state
-    bool show_demo_window = true;
-
-    glm::vec3 translationA(200, 200, 0);
-    glm::vec3 translationB(400, 200, 0);
-
-    // Animation variables
-    float r = 0.0f;
-    float increment = 1.0f;
+    test::TestClearColor test;
     
-    auto lastTime = std::chrono::high_resolution_clock::now();
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        auto currentTime = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<float> deltaTime = currentTime - lastTime;
-        lastTime = currentTime;
-
         /* Render here */
         renderer.Clear();
+
+        test.OnUpdate(0.0f);
+        test.OnRender();
 
         // Start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-
-
-        {
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-            glm::mat4 mvp = proj * view * model;
-            shader.Bind(); 
-            shader.SetUniformMat4f("u_MVP", mvp);
-
-            renderer.Draw(va, ib, shader);
-        }
-
-        {
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-            glm::mat4 mvp = proj * view * model;
-            shader.Bind(); 
-            shader.SetUniformMat4f("u_MVP", mvp);
-
-            renderer.Draw(va, ib, shader);
-        }
-        
-        // Animation
-        if (r > 1.0f)
-            increment = -1.0f;
-        else if (r < 0.0f)
-            increment = 1.0f;
-        
-        r += increment * deltaTime.count();
-
-        // ImGui window
-        // ImGui::ShowDemoWindow(&show_demo_window);
-        {
-            static float f = 0.0f;
-
-            ImGui::SliderFloat("TranslationA-X", &translationA.x, 0.0f, 960.0f);
-            ImGui::SliderFloat("TranslationA-Y", &translationA.y, 0.0f, 540.0f);
-
-            ImGui::SliderFloat("TranslationB-X", &translationB.x, 0.0f, 960.0f);
-            ImGui::SliderFloat("TranslationB-Y", &translationB.y, 0.0f, 540.0f);
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-        }
-
+       
         // ImGui rendering
+        test.OnImGuiRender();
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
